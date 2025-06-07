929e359e0243b6557f586c1770aa30e8440c3fa3
yaroslav7522 <shop-lviv@ukr.net>
2025-06-07 14:25:27 +0300

    updates 20250607 bot

--- app/Bot/TelegramBot.php
+++ app/Bot/TelegramBot.php
@@ -4,14 +4,18 @@ namespace App\Bot;
 
 use App\Bot\Handlers\MessageHandler;
 use App\Bot\Handlers\CallbackQueryHandler;
+use Illuminate\Support\Facades\Log;
 
 class TelegramBot
 {
     public function handle(array $update): void
     {
+		Log::debug('Telegram input call');
         if (isset($update['message'])) {
+			Log::debug('Telegram input Message');
             app(MessageHandler::class)->handle($update['message']);
         } elseif (isset($update['callback_query'])) {
+			Log::debug('Telegram input callback_query');
             app(CallbackQueryHandler::class)->handle($update['callback_query']);
         }
     }
--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Task;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 
 class TaskController extends Controller
 {
     public function index(): JsonResponse
     {
+		Log::debug('Gat all tasks.');
         $tasks = Task::all();
         return response()->json($tasks);
     }
