17859a1e3052566ff767ee58b556186dc8f67de6
yaroslav7522 <shop-lviv@ukr.net>
2025-06-07 22:52:18 +0300

    updates 20250607

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/', function (Request $request, TelegramBot $bot) {
+/*Route::post('/', function (Request $request, TelegramBot $bot) {
     $bot->handle($request->all());
     return response()->json(['ok' => true]);
-});
+});*/
 
 
 //Route::get('/tasks', [TasksController::class, 'index']);
