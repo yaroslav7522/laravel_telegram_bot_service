de28babd14994e9dfdd9939872ceb326b8d0315b
yaroslav7522 <shop-lviv@ukr.net>
2025-06-08 22:26:21 +0300

    updates 20250608 curl

--- app/Bot/TelegramApi.php
+++ app/Bot/TelegramApi.php
@@ -15,12 +15,36 @@ class TelegramApi
 
     public function sendMessage(array $params): void
     {
-        Http::post("{$this->telegram_bot_url}/sendMessage", $params);
+        //Http::post("{$this->telegram_bot_url}/sendMessage", $params);
+		$this->send_request("{$this->telegram_bot_url}/sendMessage", "post", $params);
     }
 
     public function answerCallbackQuery(array $params): void
     {
         Http::post("{$this->telegram_bot_url}/answerCallbackQuery", $params);
     }
+	
+	private function send_request($url, $method, $params = []){
+		if(!empty($params)){
+			$url = $url . $method . '?' . http_build_query($params);
+		}else{
+			$url = $url . $method;
+		}
+
+		return getSslPage($url);
+	}
+
+	private getSslPage($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_REFERER, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		return $result;
+	}	
 	
 }
